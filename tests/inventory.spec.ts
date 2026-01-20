4ac16b439389f213d0a83ab6c7867187a1a1ccb2
MorelJames <jamesmorel1234@gmail.com>
2026-01-20 10:14:11 +0100

    test: item sorting by name and price

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login'
 import { InventoryPage } from '../pages/inventory'
-import { CartPage } from '../pages/cart'
-import { CheckoutPage } from '../pages/checkout'
 import { env } from './helpers/env'
 
 test.describe('inventory test', () => {
@@ -26,5 +24,45 @@ test.describe('inventory test', () => {
         await Inventory.RemoveItemFromCart(env.products[0])
         await expect(page.locator('[data-test="add-to-cart-' + env.products[0] + '"]')).toBeVisible()
     })
+
+    test('inventory items are sorted by name (A to Z)', async ({ page }) => {
+        await page.selectOption('.product_sort_container', 'az');
+
+        const itemNames = await page.locator('.inventory_item_name').allTextContents()
+        expect(itemNames.length).toBeGreaterThan(1);
+        const expectedSortedNames = [...itemNames].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        expect(itemNames).toEqual(expectedSortedNames);
+    });
+
+    test('inventory items are sorted by name (Z to A)', async ({ page }) => {
+        await page.selectOption('.product_sort_container', 'za');
+
+        const itemNames = await page.locator('.inventory_item_name').allTextContents()
+        expect(itemNames.length).toBeGreaterThan(1);
+        const expectedSortedNames = [...itemNames].sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()))
+        expect(itemNames).toEqual(expectedSortedNames);
+    });
+
+    test('inventory items are sorted by price (low to high)', async ({ page }) => {
+        await page.pause()
+        await page.selectOption('.product_sort_container', 'lohi');
+
+        const priceTexts = await page.locator('.inventory_item_price').allTextContents();
+        const prices = priceTexts.map(text => parseFloat(text.replace('$', '').trim()));
+        expect(prices.length).toBeGreaterThan(1);
+        const expectedSortedPrices = [...prices].sort((a, b) => a - b);
+        expect(prices).toEqual(expectedSortedPrices);
+    });
+
+    test('inventory items are sorted by price (high to low)', async ({ page }) => {
+        await page.pause()
+        await page.selectOption('.product_sort_container', 'hilo');
+
+        const priceTexts = await page.locator('.inventory_item_price').allTextContents();
+        const prices = priceTexts.map(text => parseFloat(text.replace('$', '').trim()));
+        expect(prices.length).toBeGreaterThan(1);
+        const expectedSortedPrices = [...prices].sort((a, b) => b - a);
+        expect(prices).toEqual(expectedSortedPrices);
+    });
 })
 
